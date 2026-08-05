706b0c998442c9f5385f8bfa5d338d6879a68097
bouzerda0 <bouzerdaayman6@gmail.com>
2026-08-05 16:03:29 +0100

    backend : code cleaning

--- backend/internal/posts/service.go
+++ backend/internal/posts/service.go
@@ -1,8 +1,10 @@
 package posts
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"real-time-forum/database"
 	"real-time-forum/internal/models"
@@ -58,13 +60,20 @@ func checkCategories(categories []string, selected []string) bool {
 func GetUserID(r *http.Request) (int, error) {
 	cookie, err := r.Cookie("session_token")
 	if err != nil {
-		return 0, err
+		return 0, fmt.Errorf("no cookie found")
 	}
 
 	var userID int
-	err = database.DB.QueryRow("SELECT user_id FROM user_sessions WHERE session_token = ?", cookie.Value).Scan(&userID)
+	var expiresAt time.Time
+
+	err = database.DB.QueryRow("SELECT user_id, expires_at FROM user_sessions WHERE session_token = ?", cookie.Value).Scan(&userID, &expiresAt)
 	if err != nil {
-		return 0, err
+		return 0, fmt.Errorf("invalid session token")
 	}
+
+	if time.Now().After(expiresAt) {
+		return 0, fmt.Errorf("session expired")
+	}
+
 	return userID, nil
 }
